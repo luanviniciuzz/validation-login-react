b63ae5ca612e49d751391c1fe1c3553015c18d35
Luan Vinícius <luanviniciuzz@gmail.com>
2024-05-18 14:58:21 -0300

    configure hook for profile

---
+++ src/services/useHookUserData.ts
@@ -0,0 +1,96 @@
+import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import axios from './ApiService';
+
+interface Props {
+  tokens: {
+    access: string;
+    refresh: string;
+  };
+  user: {
+    created: string;
+    id: number;
+    is_active: boolean;
+    modified: string;
+    role: string;
+    type: string;
+    name: string;
+    email: string;
+    avatar: string | null;
+  };
+}
+
+interface UserDataProps {
+  name: string;
+  email: string;
+  avatar: string | null;
+}
+
+const useHookUserData = () => {
+  const [loading, setLoading] = useState(false);
+  const navigate = useNavigate();
+
+  const login = async (email: string, password: string) => {
+    try {
+      setLoading(true);
+      const response = await axios.post('/auth/login/',
+        {
+           email,
+           password
+        },
+        {
+          headers: {
+            Accept: 'application/json;version=v1_web',
+            'Content-Type': 'application/json',
+          },
+        },
+      );
+
+      const loginResponse: Props = await response.data;
+      const loggedUserToken = loginResponse.tokens.access;
+
+      localStorage.setItem('loggedUserToken', loggedUserToken);
+
+    } catch (error) {
+     
+    } finally {
+      setLoading(false);
+    }
+  };
+  const reloadLogin = (token: string | null) => {
+    if (token) {
+      navigate('/profile');
+    }
+  };
+
+  const searchUserData = async (token: string | null) => {
+    try {
+      setLoading(true);
+      if (!token) {
+        navigate('/');
+        return null;
+      }
+
+      const response = await axios.get('/auth/profile/',
+        {
+          headers: {
+            Accept: 'application/json;version=v1_web',
+            'Content-Type': 'application/json',
+            Authorization: `Bearer ${token}`,
+          },
+        },
+      );
+
+      const userData: UserDataProps = await response.data;
+      return userData;
+    } catch (error) {
+     
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  return { searchUserData, loading, login, reloadLogin };
+};
+
+export default useHookUserData;
